dca49a7cbcae5c5d907c2f525796555279a3bd1e
nicholas <nicholas.lazzerini@abstract.it>
2024-08-02 10:08:41 +0200

    first commit

---
+++ raycast-env.d.ts
@@ -0,0 +1,30 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** Default Action - The default action to perform. */
+  "defaultAction": "copy" | "paste",
+  /** Area Code - Include the area code in the phone number. */
+  "includeAreaCode": boolean
+}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `index` command */
+  export type Index = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `index` command */
+  export type Index = {}
+}
+
+
